990ef97bd40bb33fa35f2ff05af0ea3d9c0d7044
FuXI711 <bcx15961292359@gmail.com>
2025-07-30 08:27:00 +0800

    区块链读写

---
+++ task5/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://sepolia.infura.io/v3/cef15f4b47934f068d6d15ff6ce629e8")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(5671744)
+
+	header, err := client.HeaderByNumber(context.Background(), blockNumber)
+	fmt.Println(header.Number.Uint64())     // 5671744
+	fmt.Println(header.Time)                // 1712798400
+	fmt.Println(header.Difficulty.Uint64()) // 0
+	fmt.Println(header.Hash().Hex())        // 0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(block.Number().Uint64())
+	fmt.Println(block.Time())
+	fmt.Println(block.Difficulty().Uint64())
+	fmt.Println(block.Nonce())
+	fmt.Println(block.GasLimit())
+	fmt.Println(block.Hash().Hex())
+	fmt.Println(len(block.Transactions()))
+	count, err := client.TransactionCount(context.Background(), block.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(count) // 70
+}
+
+//a3189fa9381849f460e4ed8b04c0d5c055df81d9b16b6eabe0a597bbf93ef6f9
